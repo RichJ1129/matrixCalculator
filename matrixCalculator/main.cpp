f9241a90f61a3afa43b6cb1bca955a0a389d7eda
Richard Joseph <josephri@oregonstate.edu>
2019-04-01 20:49:53 -0400

    Initial Commit

---
+++ matrixCalculator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  matrixCalculator
+//
+//  Created by Richard Joseph on 4/1/19.
+//  Copyright © 2019 Richard Joseph. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
